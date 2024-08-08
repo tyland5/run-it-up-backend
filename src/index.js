279820f611ae24ceab7c2af15898c5976e2db76b
Tyler <tyler.landivar@gmail.com>
2024-08-08 11:48:12 -0400

    allow for static hosting images

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const login = require('../src/login/login');
 
 app.use('/login', login);
 
+app.use(express.static('public'));
+app.use("/images", express.static('images'))
+
 app.listen(8080, () => {
     console.log('server listening on port 8080')
 })
